08de00d006e527ae5c8b4278a1495667235af3f7
Alexandr Voishel <avoishel@gmail.com>
2022-01-15 14:42:37 +0300

    add: scroling app

---
+++ app/src/main/java/android/examample/imageloader_hw_31/MainActivity_HW_2_2.java
@@ -0,0 +1,14 @@
+package android.examample.imageloader_hw_31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_HW_2_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hw22);
+    }
+}
