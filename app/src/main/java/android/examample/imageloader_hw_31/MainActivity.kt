e016ec323a05e0e1c9a9b406618701a0b54c7815
Alexandr Voishel <avoishel@gmail.com>
2022-01-19 16:33:12 +0300

    add: night theam switcher

--- app/src/main/java/android/examample/imageloader_hw_31/MainActivity.kt
+++ app/src/main/java/android/examample/imageloader_hw_31/MainActivity.kt
@@ -9,13 +9,26 @@ import android.examample.imageloader_hw_31.HomeWorck_2.MainActivity_HW_2_3
 import android.examample.imageloader_hw_31.HomeWorck_3.MainActivity_HW_3_1
 import android.examample.imageloader_hw_31.HomeWorck_3.MainActivity_HW_3_2
 import android.examample.imageloader_hw_31.HomeWorck_4.MainActivity_HW_4
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var nightTeamSwitch: Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nightTeamSwitch = findViewById(R.id.nightTheamSwitch)
+        nightTeamSwitch.setOnClickListener {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        }
     }
 
     fun goToHomework_3_1(view: android.view.View) {
